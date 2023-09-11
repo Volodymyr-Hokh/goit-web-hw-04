9ac9acbcd7041f9bc604e38d8079d19f69fe5d65
Volodymyr-Hokh <gokh.00@ukr.net>
2023-09-11 11:46:49 +0300

    Add socket server

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+from datetime import datetime
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
 import mimetypes
 from pathlib import Path
+import socket
+from threading import Thread   
 import urllib.parse
+
+
+SOCKET_IP = "127.0.0.1"
+SOCKET_PORT = 5000
 
 
 class HttpHandler(BaseHTTPRequestHandler):
@@ -36,10 +44,54 @@ class HttpHandler(BaseHTTPRequestHandler):
         self.end_headers()
         with open(file_path, 'rb') as file:
             self.wfile.write(file.read())
-        
+
+    def do_POST(self):
+        data = self.rfile.read(int(self.headers['Content-Length']))
+        send_data_to_socket(data)
+        self.send_response(302)
+        self.send_header('Location', '/')
+        self.end_headers()
 
 
-def run(server_class=HTTPServer, handler_class=HttpHandler):
+
+def save_data(data):
+    data_parse = urllib.parse.unquote_plus(data.decode())
+
+    data_path = Path("storage").joinpath("data.json")
+
+    with open(data_path, encoding="utf-8") as file:
+        try:
+            data_json = json.load(file)
+        except json.decoder.JSONDecodeError:
+            data_json = {}
+        data_json[str(datetime.now())] = {key: value for key, value in [el.split('=') for el in data_parse.split('&')]}
+
+    with open(data_path, "w", encoding="utf-8") as fh:
+        json.dump(data_json, fh, indent=4, ensure_ascii=False)
+
+
+def send_data_to_socket(data):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(data, (SOCKET_IP, SOCKET_PORT))
+    sock.close()
+
+
+def run_socket_server(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server = ip, port
+    sock.bind(server)
+    try:
+        while True:
+            data, address = sock.recvfrom(1024)
+            save_data(data)
+
+    except KeyboardInterrupt:
+        print(f'Destroy server')
+    finally:
+        sock.close()
+
+
+def run_http_server(server_class=HTTPServer, handler_class=HttpHandler):
     server_address = ('', 3000)
     http = server_class(server_address, handler_class)
     try:
@@ -49,4 +101,11 @@ def run(server_class=HTTPServer, handler_class=HttpHandler):
 
 
 if __name__ == '__main__':
-    run()
+    http_server_thread = Thread(target=run_http_server)
+    http_server_thread.start()
+
+    socket_server_thread = Thread(target=run_socket_server, args=(SOCKET_IP, SOCKET_PORT))
+    socket_server_thread.start()
+
+    http_server_thread.join()
+    socket_server_thread.join()
